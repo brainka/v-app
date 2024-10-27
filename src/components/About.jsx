8a1267cd124b3e43de1c2a86d0bd0a2b8b68e6d7
brainka <brainkarau@gmail.com>
2024-10-27 20:55:47 +1100

    add about me image

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import './About.css';
 import { useState } from 'react';
 
 const About = () => {
-	return <div>About me</div>;
+	return <div className="img-container mt-64">Hello! I'm Vernon :)</div>;
 };
 
 export default About;
